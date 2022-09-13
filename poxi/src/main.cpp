b9d3d141940cb0d91bc7cd507b9887873e5202ad
wheatfox <enkerewpo@hotmail.com>
2022-09-13 19:12:09 +0800

    build success

--- poxi/src/main.cpp
+++ poxi/src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
+#include "HAL/HAL.h"
+#include "Port/Display.h"
+#include "App/App.h"
 
-#define LED_PIN 16
-
-void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  Serial.begin(115200);
-  Serial.println("");
-  Serial.println("Hello World!");
-  Serial.println("This is my first esp8266 code!");
+void setup()
+{
+    HAL::Init();
+    Port_Init();
+    App_Init();
 }
 
-void loop() {
-  digitalWrite(LED_PIN, LOW);
-  delay(1000);
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
+void loop()
+{
+    HAL::Update();
+
+    delay(20);
 }
